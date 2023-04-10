1dbbf3b38c11e9c452779ca973c3160c88c943c6
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2023-04-10 15:19:22 +0300

    Setup tauri

--- flake.nix
+++ flake.nix
@@ -4,9 +4,13 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+    rust-overlay.inputs.flake-utils.follows = "flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
     let
       supportedSystems = [
         "aarch64-darwin"
@@ -19,13 +23,23 @@
       let
         pkgs = import nixpkgs {
           inherit system;
+          overlays = [
+            (import rust-overlay)
+          ];
         };
+
+        inherit (pkgs) lib;
       in
       rec {
         devShell = pkgs.mkShell {
           packages = [
+            pkgs.rust-bin.stable.latest.default
             pkgs.nodejs
             pkgs.nodePackages.pnpm
+          ] ++ lib.optionals pkgs.stdenv.isDarwin [
+            pkgs.darwin.apple_sdk.frameworks.AppKit
+            pkgs.darwin.apple_sdk.frameworks.Carbon
+            pkgs.darwin.apple_sdk.frameworks.WebKit
           ];
         };
       });
